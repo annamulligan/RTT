43776e509aafada0a8111ecdfdeaed649a6e6bca
ajmul <ajmulligan27@gmail.com>
2017-01-31 21:27:59 +1300

    Wrote php to create a simple representation of the static data db

---
+++ create_static.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ajmul
+ * Date: 1/31/2017
+ * Time: 9:19 PM
+ */
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "RTT";
+
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
+    $sql = "CREATE DATABASE RTT";
+    if ($conn->query($sql) === TRUE) {
+        echo "Database created successfully";
+    } else {
+        echo "Error creating database: " . $conn->error;
+    }
+    $conn->close();
+
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // sql to create table
+    $sql = "CREATE TABLE StaticData (
+    eventid INT(6) PRIMARY KEY, 
+    CaseOwner VARCHAR(30) NOT NULL,
+    Age Int(6) NOT NULL
+    )";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Table StaticData created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+
+
+
+    $sql = "INSERT INTO StaticData (eventid, CaseOwner, Age)
+    VALUES (22,'John Smith', 16)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $sql = "INSERT INTO StaticData (eventid, CaseOwner, Age)
+        VALUES (24,'John Smith', 18)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $sql = "INSERT INTO StaticData (eventid, CaseOwner, Age)
+        VALUES (212,'John Smith', 45)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+
+?>
