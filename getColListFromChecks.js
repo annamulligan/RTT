0d3f202c61772c6ca646be91f2ef02ed8d924693
ajmul <ajmulligan27@gmail.com>
2017-02-02 18:28:40 +1300

    made the checkboxes on the report cols tab affect the displayed columns in the grid

---
+++ getColListFromChecks.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ajmul on 2/2/2017.
+ */
+function getCols(){
+    var checkBoxListId = "ReportColsList"
+    var elementRef = document.getElementById(checkBoxListId);
+    var checkBoxArray = elementRef.getElementsByTagName('input');
+    var selections = [];
+    var count = 0;
+    for (var i =0; i <checkBoxArray.length; i++){
+        if(checkBoxArray[i].checked==true){
+            selections[count] = checkBoxArray[i].value;
+            count++;
+        }
+    }
+    //alert(selections.length);
+    return selections;
+}
